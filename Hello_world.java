45f0f0a6b2598ad0522cb43e0a36df086f62db1c
Step-Sea <stepwork135@gmail.com>
2026-02-13 13:57:09 +0400

    hello world

---
+++ Hello_world.java
@@ -0,0 +1,6 @@
+class Hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test of the system.");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OOSD-Assignment
+
+Testing~
