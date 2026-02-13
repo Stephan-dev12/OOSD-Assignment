6ca2a9be290942cb28d5e1f4e85a86b37f1fe592
Step-Sea <150064626+Step-Sea@users.noreply.github.com>
2026-02-13 16:57:13 +0400

    Add new integer variables num3 and num4
    Added two new integer variables for future calculations.

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,9 @@ public class Calculator {
     public static void main(String[] args) {
         String num1 = (System.console().readLine());
         int num2 = 10;
-
+        int num3 = 10000;
+        int num4 = 1026;
+        
         int sum = Integer.parseInt(num1) + num2;
         int difference = Integer.parseInt(num1) - num2;
         int quotient = Integer.parseInt(num1) / num2;
